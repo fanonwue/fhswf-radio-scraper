5267a5dcca4ff350608e500165e35a6f95b78bcc
Nils Robinet <nils.robinet@gmail.com>
2025-04-20 14:43:57 +0200

    Add crawlers for WDR2 and 1Live that extract the song playlist data into json files

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.reactor import install_reactor
 from TestSpider import TestSpider, TestSpider2
+from WdrSpider import Wdr1Spider, Wdr2Spider
 from settings import SETTINGS
 
 def run():
     install_reactor('twisted.internet.asyncioreactor.AsyncioSelectorReactor')
     process = CrawlerProcess(SETTINGS)
+    process.crawl(Wdr1Spider)
+    process.crawl(Wdr2Spider)
     process.crawl(TestSpider)
     process.crawl(TestSpider2)
     process.start()
