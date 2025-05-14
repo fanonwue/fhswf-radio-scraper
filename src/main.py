04b8a1a047b121eddf8ebb91b31d265f547e41be
smilchsack <sebastian@milchsack.com>
2025-05-14 21:48:31 +0200

    Landing Pages for SRF3 & SWR3.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from datetime import date, datetime
 import SRF3PlaylistSpider
 import SWR3PlaylistSpider
+import SWR3LandingPage
+import SRF3LandingPage
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.reactor import install_reactor
 from settings import SETTINGS
@@ -10,11 +12,16 @@ def run():
     process = CrawlerProcess(SETTINGS)
     
     # Set the start and end dates for the playlist retrieval
+    # @To-Do: Download once a day, for the previous day.
     start_date = date.today().strftime("%Y-%m-%d")
     end_date = None
-    
     process.crawl(SWR3PlaylistSpider.SWR3PlaylistSpider, start_date_param=start_date, end_date_param=end_date)
     process.crawl(SRF3PlaylistSpider.SRF3PlaylistSpider, start_date_param=start_date, end_date_param=end_date)
+    
+    # @To-Do: Now: E.g. download hourly to collect the current host, headlines etc.
+    process.crawl(SWR3LandingPage.SWR3LandingPage)
+    process.crawl(SRF3LandingPage.SRF3LandingPage)
+
     process.start()
 
 if __name__ == '__main__':
